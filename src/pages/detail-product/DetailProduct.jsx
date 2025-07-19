8fa65141e809f8b138f2f4b51f56cf0e887d4289
kalgo4444 <abuxyziabd@gmail.com>
2025-07-19 17:30:06 +0500

    abdulaziz

--- src/pages/detail-product/DetailProduct.jsx
+++ src/pages/detail-product/DetailProduct.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailProduct = () => {
+
+  const { id } = useParams()
   return (
-    <div>DetailProduct</div>
+    <div>Abdulaziz Section</div>
   )
 }
 
