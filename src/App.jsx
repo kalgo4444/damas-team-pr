c726122220cdca19a9f17ff115950e4ff33f106e
kalgo4444 <abuxyziabd@gmail.com>
2025-07-26 17:20:59 +0500

    add blog

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,25 @@ import DetailProduct from "./pages/detail-product/DetailProduct";
 import Wishlist from "./pages/wishlist/Wishlist";
 import Cart from "./pages/cart/Cart";
 import Contact from "./pages/contact/Contact";
-import About from "./pages/about/About";
+import About from "./pages/about/Blog";
+import Blog from "./pages/about/Blog";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index={true} element={<Home />} />
-					<Route path='/wishlist' element={<Wishlist />} />
-					<Route path='/cart' element={<Cart />} />
-					<Route path='/contact' element={<Contact />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/product/:id' element={<DetailProduct />} />
-				</Route>
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index={true} element={<Home />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/product/:id" element={<DetailProduct />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
