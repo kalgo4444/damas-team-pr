bf84f7a8ac8dd06a3d25dddda3eae84f8c459419
kalgo4444 <abuxyziabd@gmail.com>
2025-07-31 14:38:40 +0500

    df

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// axios instance
 export const api = axios.create({
     baseURL: "https://dummyjson.com"
 })
 
-// api.interceptors.request.use(()=>{})
+
--- src/zustand/useCart.jsx
+++ src/zustand/useCart.jsx
@@ -1,35 +1,35 @@
 import { create } from "zustand";
 
-const toLocal = velue => localStorage.setItem("cart", JSON.stringify(velue));
+const toLocal = (velue) => localStorage.setItem("cart", JSON.stringify(velue));
 
-export const useCart = create(set => ({
-	cart: JSON.parse(localStorage.getItem("cart")) || [],
-	toggleToCart: payload =>
-		set(state => {
-			const ex = state.cart.some(item => item.id === payload.id);
-			let store = [];
-			if (ex) {
-				store = state.cart.filter(item => item.id !== payload.id);
-			} else {
-				store = [...state.cart, { ...payload, quantity: 1 }];
-			}
-			toLocal(store);
-			return { cart: store };
-		}),
-	incCart: payload =>
-		set(state => {
-			let store = state.cart.map(item =>
-				item.id === payload.id ? { ...item, quantity: item.quantity + 1 } : item
-			);
-			toLocal(store);
-			return { cart: store };
-		}),
-	decCart: payload =>
-		set(state => {
-			let store = state.cart.map(item =>
-				item.id === payload.id ? { ...item, quantity: item.quantity - 1 } : item
-			);
-			toLocal(store);
-			return { cart: store };
-		}),
+export const useCart = create((set) => ({
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
+  toggleToCart: (payload) =>
+    set((state) => {
+      const ex = state.cart.some((item) => item.id === payload.id);
+      let store = [];
+      if (ex) {
+        store = state.cart.filter((item) => item.id !== payload.id);
+      } else {
+        store = [...state.cart, { ...payload, quantity: 1 }];
+      }
+      toLocal(store);
+      return { cart: store };
+    }),
+  incCart: (payload) =>
+    set((state) => {
+      let store = state.cart.map((item) =>
+        item.id === payload.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      toLocal(store);
+      return { cart: store };
+    }),
+  decCart: (payload) =>
+    set((state) => {
+      let store = state.cart.map((item) =>
+        item.id === payload.id ? { ...item, quantity: item.quantity - 1 } : item
+      );
+      toLocal(store);
+      return { cart: store };
+    }),
 }));
